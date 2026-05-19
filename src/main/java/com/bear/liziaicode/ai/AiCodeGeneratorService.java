6f189bf8778fc0b3f2fb28d524521d2c359431bc
王雄 <wangxiong@U-T794C6FQ-2315.local>
2026-05-19 14:47:00 +0800

    解析识别网页代码，单元测试

--- src/main/java/com/bear/liziaicode/ai/AiCodeGeneratorService.java
+++ src/main/java/com/bear/liziaicode/ai/AiCodeGeneratorService.java
@@ -3,6 +3,7 @@ package com.bear.liziaicode.ai;
 import com.bear.liziaicode.ai.model.HtmlCodeResult;
 import com.bear.liziaicode.ai.model.MultiFileCodeResult;
 import dev.langchain4j.service.SystemMessage;
+import reactor.core.publisher.Flux;
 
 //服务接口
 public interface AiCodeGeneratorService {
@@ -24,5 +25,23 @@ public interface AiCodeGeneratorService {
      */
     @SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
     MultiFileCodeResult generateMultiFileCode(String userMessage);
+
+    /**
+     * 生成 HTML 代码（流式）
+     *
+     * @param userMessage 用户消息
+     * @return 生成的代码结果
+     */
+    @SystemMessage(fromResource = "prompt/codegen-html-system-prompt.txt")
+    Flux<String> generateHtmlCodeStream(String userMessage);
+
+    /**
+     * 生成多文件代码（流式）
+     *
+     * @param userMessage 用户消息
+     * @return 生成的代码结果
+     */
+    @SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
+    Flux<String> generateMultiFileCodeStream(String userMessage);
 
 }
